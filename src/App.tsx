7c29c7fc5ae04882d009f8707f909b3794a674f1
Elona Kajtazi <MY_NAME@example.com>
2022-08-19 15:23:18 +0300

    I added some more stuff ...

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,55 @@ import "./App.css";
 
 function App() {
   const [dice, setDice] = useState(0);
-  const [diceCount, setDiceCount] = useState(0);
+  // const [diceCount, setDiceCount] = useState(0 + dice);
   // const [player, setPlayer] = useState(true);
   function rollDice() {
     setDice(Math.floor(Math.random() * 6) + 1);
   }
+
   return (
     <div className="App">
-      <h1>Dice: {dice}</h1>
+      <h1>
+        {dice === 0 ? <h2>Roll the dice </h2> : null}
+
+        {dice === 1 ? (
+          <img
+            src="https://thefiveplanets.org/b01/data/graphics/textures/dice/face1.jpg"
+            alt=""
+          />
+        ) : null}
+        {dice === 2 ? (
+          <img
+            src="https://thefiveplanets.org/b01/data/graphics/textures/dice/face2.jpg"
+            alt=""
+          />
+        ) : null}
+        {dice === 3 ? (
+          <img
+            src="https://thefiveplanets.org/b01/data/graphics/textures/dice/face3.jpg"
+            alt=""
+          />
+        ) : null}
+        {dice === 4 ? (
+          <img
+            src="https://thefiveplanets.org/b01/data/graphics/textures/dice/face4.jpg"
+            alt=""
+          />
+        ) : null}
+        {dice === 5 ? (
+          <img
+            src="https://thefiveplanets.org/b01/data/graphics/textures/dice/face5.jpg"
+            alt=""
+          />
+        ) : null}
+        {dice === 6 ? (
+          <img
+            src="https://thefiveplanets.org/b01/data/graphics/textures/dice/face6.jpg"
+            alt=""
+          />
+        ) : null}
+      </h1>
+      <p>You got: {dice}</p>
       <button
         onClick={() => {
           rollDice();
